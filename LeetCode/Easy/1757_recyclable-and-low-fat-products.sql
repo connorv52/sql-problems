7680bcf787da8ee9c7c3d2ecfa5fd97f6a22d166
Connor Valle <81936436+connorv52@users.noreply.github.com>
2025-07-31 16:36:06 -0700

    Update 1757_recyclable-and-low-fat-products.sql

--- LeetCode/Easy/1757_recyclable-and-low-fat-products.sql
+++ LeetCode/Easy/1757_recyclable-and-low-fat-products.sql
@@ -1,33 +1,59 @@
--- Title: Recyclable and Low Fat Products
--- Problem ID: 1757
--- Difficulty: Easy
--- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
--- Author: Connor Valle
--- 
--- Schema: Products(product_id INT, low_fats ENUM('Y', 'N'), recyclable ENUM('Y', 'N'))
---
--- Description: Write a SQL query to find the ids of products that are both low fat and recyclable. Return the result table in any order.
---
--- Example 1:
--- Input:
--- +-------------+----------+------------+
--- | product_id  | low_fats | recyclable |
--- +-------------+----------+------------+
--- | 0           | Y        | N          |
--- | 1           | Y        | Y          |
--- | 2           | N        | Y          |
--- | 3           | Y        | Y          |
--- | 4           | N        | N          |
--- +-------------+----------+------------+
--- Output:
--- +-------------+
--- | product_id  |
--- +-------------+
--- | 1           |
--- | 3           |
--- +-------------+
+--Title: Recyclable and Low Fat Products
+--Problem ID: 1757
+--Difficulty: Easy
+--Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+--Author: Connor Valle
 
--- ✅ Solution:
+--Table: Products
+
+--+-------------+---------+
+--| Column Name | Type    |
+--+-------------+---------+
+--| product_id  | int     |
+--| low_fats    | enum    |
+--| recyclable  | enum    |
+--+-------------+---------+
+--product_id is the primary key (column with unique values) for this table.
+--low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+--recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+
+ 
+
+--Write a solution to find the ids of products that are both low fat and recyclable.
+
+--Return the result table in any order.
+
+--The result format is in the following example.
+
+ 
+
+--Example 1:
+
+--Input: 
+--Products table:
+--+-------------+----------+------------+
+--| product_id  | low_fats | recyclable |
+--+-------------+----------+------------+
+--| 0           | Y        | N          |
+--| 1           | Y        | Y          |
+--| 2           | N        | Y          |
+--| 3           | Y        | Y          |
+--| 4           | N        | N          |
+--+-------------+----------+------------+
+--Output: 
+--+-------------+
+--| product_id  |
+--+-------------+
+--| 1           |
+--| 3           |
+--+-------------+
+--Explanation: Only products 1 and 3 are both low fat and recyclable.
+
+
+
+
+
+--✅ Solution:
 SELECT product_id
 FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y';
