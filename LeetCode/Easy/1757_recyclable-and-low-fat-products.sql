44d25260028229577794b32ca57f8e2b436395be
Connor Valle <81936436+connorv52@users.noreply.github.com>
2025-07-31 16:17:31 -0700

    Create 1757_recyclable-and-low-fat-products.sql

---
+++ LeetCode/Easy/1757_recyclable-and-low-fat-products.sql
@@ -0,0 +1,33 @@
+-- Title: Recyclable and Low Fat Products
+-- Problem ID: 1757
+-- Difficulty: Easy
+-- Source: https://leetcode.com/problems/recyclable-and-low-fat-products/
+-- 
+-- Schema:
+-- Products(product_id INT, low_fats ENUM('Y', 'N'), recyclable ENUM('Y', 'N'))
+--
+-- Write a SQL query to find the ids of products that are both low fat and recyclable.
+--
+-- Example:
+-- Input:
+-- +-------------+----------+------------+
+-- | product_id  | low_fats | recyclable |
+-- +-------------+----------+------------+
+-- | 0           | Y        | N          |
+-- | 1           | Y        | Y          |
+-- | 2           | N        | Y          |
+-- | 3           | Y        | Y          |
+-- | 4           | N        | N          |
+-- +-------------+----------+------------+
+-- Output:
+-- +-------------+
+-- | product_id  |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
+
+-- ✅ Solution:
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
